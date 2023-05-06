c6418a641f6db982c8415a8ea020fd2f0d2bff48
Yemima Sutanto <64170347+pootreth@users.noreply.github.com>
2023-05-06 17:05:51 +0700

    quiz1

---
+++ src/components/Quiz.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export default function Quiz(){
+    const [quizzes, setQuizzes] = useState([])
+
+    useEffect(()=>{
+        axios.get('https://opentdb.com/api.php?amount=5&type=multiple').then(res => setQuizzes(res.data.results))
+        
+    }, [])
+
+    console.log(quizzes)
+
+    return (
+        <>
+            {
+                quizzes.map(quiz => <div key={crypto.randomUUID()}>
+                    {quiz.question}
+                </div>)
+            }
+        </>
+
+    )
+}
